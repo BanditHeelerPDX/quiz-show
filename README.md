011deb15c79ac86b792302a3ee0a95c46b46f065
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-02-20 20:56:47 -0800

    initial commit - file creation, loose readme and a handful of quiz questions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # quiz-show
+
+## Description
+
+Test your might, your will, ok, just your knowledge of Javascript.  60 seconds to answer as many questions correctly as you can.  Leave your initials to let the rest of us know who did their homework.
---
+++ script.js
@@ -0,0 +1,34 @@
+
+// Questions for the quiz - or the quizQuestion array, if you will
+const quizQuestions = [  {    
+    question: "Which of the following will not properly create an array?",    
+    choices: ["let oldArray = new Array[16]", "var myarray = ['Dwayne', 16, 32, 48]", "let yourarray = myarray()", "var thearray = vacuums('hoover', 'dyson', 'magnavox')"],
+    answer: "let oldArray = new Array[16]"
+  },
+  {
+    question: "What type of programming language is Javascript?",
+    choices: ["Object-focused", "Superficial", "Object-oriented", "Vanilla"],
+    answer: "Object-oriented"
+  },
+  {
+    question: "Which of the following is not a property of the navigator object?",
+    choices: ["appName", "appCodeName", "appType", "appVersion"],
+    answer: "appType"
+  },
+  {
+    question: "Which of these is not a method of the Date object?",
+    choices: ["getDate()", "getHours()", "getTimezoneOffset()", "getShorty()"],
+    answer: "getShorty()"
+  },
+  {
+    question: "To resize a window relative to its current size, use this method:",
+    choices: ["resizeTo()", "resizeBy()", "scrollTo()", "moveBy()"],
+    answer: "resizeBy()"
+  },
+  {
+    question: "Window: message event",
+    choices: ["This event bubbles, but cannot be cancelled.", "This event can be cancelled but does not bubble.", "This event bubbles and can be cancelled.", "This event is not cancellable and does not bubble."],
+    answer: "This event is not cancellable and does not bubble."
+  },
+  // Add additional questions as required...
+];
